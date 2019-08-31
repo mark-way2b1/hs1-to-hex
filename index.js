4ba94b85625baf20216fb7d2b1c1467fdc1591ff
Mark Young <mark@way2b1.com>
2019-08-31 15:48:39 -0500

    v1.0.0

---
+++ index.js
@@ -0,0 +1,35 @@
+var toRgb = require('@btmango18/hsl-to-hex')
+
+function max (val, n) {
+  return (val > n) ? n : val
+}
+
+function min (val, n) {
+  return (val < n) ? n : val
+}
+
+function cycle (val) {
+  // for safety:
+  val = max(val, 1e7)
+  val = min(val, -1e7)
+  // cycle value
+  while (val < 0) { val += 360 }
+  while (val > 359) { val -= 360 }
+  return val
+}
+
+function hsl (hue, saturation, luminosity) {
+  // resolve degrees to 0-369 range
+  hue = cycle(hue)
+
+  // enforce constraints
+  saturation = min(max(saturation, 100), 0)
+  luminosity = min(max(luminosity, 100), 0)
+
+  console.log(hue, saturation, luminosity)
+
+  var hex = toRgb(hue, saturation, luminosity)
+  return hex
+}
+
+module.exports = hsl
